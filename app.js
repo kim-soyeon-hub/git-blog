1b33e9c54afb74e766f29742641f27f64c914215
냐옹 <rlathdus2856@naver.com>
2024-08-28 10:21:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 console.log("Hello Git!");
 console.log(1 + 2);
-<<<<<<< Updated upstream
 console.log("이순신");
 console.log(3.14 * 2); //개발자A가 수정함
-=======
 console.log("홍길동");
->>>>>>> Stashed changes
